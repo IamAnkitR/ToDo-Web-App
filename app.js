27d0f76415ac9a3f4bdb2352873d4f659bb705b4
IamAnkitR <104831310+IamAnkitR@users.noreply.github.com>
2022-12-27 00:40:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,30 +2,33 @@
 const {request, response} = require('express');
 const express = require('express');
 const app = express();
+
 const {Todo} = require('./models');
 const bodyParser = require('body-parser');
+
 app.use(express.urlencoded({extended: false}));
 const path = require('path');
+
 app.use(bodyParser.json());
+
+
 // seting the ejs is the engine
 app.set('view engine', 'ejs');
 
 app.get('/', async (request, response)=>{
   const allTodos = await Todo.getTodos();
-  const overdue = await Todo.overdue();
-  const dueToday = await Todo.dueToday();
-  const dueLater = await Todo.dueLater();
-
+  
   if (request.accepts('html')) {
     response.render('index', {
-      allTodos, overdue, dueToday, dueLater,
       allTodos
     });
   } else {
     response.json({allTodos});
   }
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/todos', (request, response)=>{
   console.log('Todo List', request.body);
 });
@@ -42,6 +45,7 @@ app.post('/todos', async (request, response)=>{
     return response.status(422).json(error);
   }
 });
+
 app.put('/todos/:id/markAsCompleted', async (request, response)=>{
   console.log('We have updated a todo with id:', request.params.id);
   const todo = await Todo.findByPk(request.params.id);
@@ -53,9 +57,11 @@ app.put('/todos/:id/markAsCompleted', async (request, response)=>{
     return response.status(422).json(error);
   }
 });
+
 app.delete('/todos/:id', async function(request, response) {
   console.log('Delete Todo with id: ', request.params.id);
   const deleteFlag = await Todo.destroy({where: {id: request.params.id}});
   response.send(deleteFlag ? true : false);
 });
+
 module.exports = app;
